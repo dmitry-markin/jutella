0657b9191b2f3b7277543df8fc239855b54e1166
Dmitry Markin <dmitry@markin.tech>
2024-09-16 21:29:07 +0000

    Support setting API key in a config

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,28 @@
 use anyhow::anyhow;
 use colored::Colorize as _;
-use std::io::{self, Write as _};
+use dirs::home_dir;
+use std::{
+    env, fs,
+    io::{self, Write as _},
+};
 use unspoken::ChatClient;
 
 fn main() -> anyhow::Result<()> {
-    let api_key =
-        std::env::var("OPENAI_API_KEY").map_err(|_| anyhow!("Please set `OPENAI_API_KEY` env."))?;
+    let api_key = env::var("OPENAI_API_KEY")
+        .or_else(|_| {
+            home_dir()
+                .map(|home| {
+                    fs::read_to_string(home.join(".config/unspoken.key"))
+                        .map(|key| key.trim().to_owned())
+                        .ok()
+                })
+                .flatten()
+                .ok_or("no home or can not read key file")
+        })
+        .map_err(|_| {
+            anyhow!("Please set `OPENAI_API_KEY`, either via env or in $HOME/.config/unspoken.key")
+        })?;
+
     let mut chat = ChatClient::new(api_key, Default::default());
 
     let you = "You:".bold().red();
