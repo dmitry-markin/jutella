8ef59ff4b8cc0719554654318fb320bfc23105a2
Dmitry Markin <dmitry@markin.tech>
2024-09-17 07:43:19 +0000

    Introduce a config file

--- src/main.rs
+++ src/main.rs
@@ -1,74 +1,132 @@
-use anyhow::anyhow;
+use anyhow::{anyhow, Context as _};
 use clap::Parser;
 use colored::Colorize as _;
 use dirs::home_dir;
 use std::{
     env, fs,
     io::{self, Write as _},
+    path::PathBuf,
 };
 use unspoken::{ChatClient, ChatClientConfig};
 
 /// OpenAI chat API command line client.
+///
+/// Command line options override config file.
 #[derive(Debug, clap::Parser)]
 #[command(version, about, long_about = None)]
 struct Args {
-    /// API url.
-    #[arg(short, long, default_value_t = String::from("https://models.inference.ai.azure.com/"))]
-    url: String,
+    /// API url. Example: "https://models.inference.ai.azure.com/".
+    #[arg(short, long)]
+    url: Option<String>,
 
-    /// Model.
-    #[arg(short, long, default_value_t = String::from("gpt-4o"))]
-    model: String,
+    /// Model. Example: "gpt-4o".
+    #[arg(short, long)]
+    model: Option<String>,
 
     /// System message to initialize the model. Example: "You are a helpful assistant."
     #[arg(short, long)]
     system: Option<String>,
+
+    /// Config file location. Default: $HOME/.config/unspoken.toml.
+    #[arg(short, long)]
+    config: Option<PathBuf>,
 }
 
-struct Configuration {
+#[derive(Debug, serde::Deserialize)]
+struct Config {
+    api_key: Option<String>,
+    url: Option<String>,
+    model: Option<String>,
+    system_message: Option<String>,
+}
+
+struct AppConfiguration {
     api_key: String,
     api_url: String,
     model: String,
     system_message: Option<String>,
 }
 
-impl Configuration {
+impl AppConfiguration {
     fn init(args: Args) -> anyhow::Result<Self> {
-        let api_key = env::var("OPENAI_API_KEY")
-            .or_else(|_| {
-                home_dir()
-                    .map(|home| {
-                        fs::read_to_string(home.join(".config/unspoken.key"))
-                            .map(|key| key.trim().to_owned())
-                            .ok()
-                    })
-                    .flatten()
-                    .ok_or("no home or can not read key file")
-            })
-            .map_err(|_| {
-                anyhow!(
-                    "Please set `OPENAI_API_KEY`, either via env or in $HOME/.config/unspoken.key"
-                )
-            })?;
+        let Args {
+            url,
+            model,
+            system,
+            config,
+        } = args;
 
-        let Args { url, model, system } = args;
+        let config: Option<Config> = if let Some(config_path) = config {
+            // Try reading CLI-provided config file first.
+            Some(
+                toml::from_str(&fs::read_to_string(config_path.clone()).with_context(|| {
+                    anyhow!(
+                        "Failed to read config file {}",
+                        config_path
+                            .to_str()
+                            .expect("to have only unicode characters in path")
+                    )
+                })?)
+                .context("Failed to parse config file {config_path}")?,
+            )
+        } else {
+            // If there is $HOME, try reading config from standard path.
+            if let Some(config_path) = home_dir().map(|home| home.join(".config/unspoken.toml")) {
+                match fs::read_to_string(config_path.clone()) {
+                    Ok(string) => Ok(toml::from_str(&string).with_context(|| {
+                        anyhow!(
+                            "Failed to parse config file {}",
+                            config_path
+                                .to_str()
+                                .expect("to have only unicode characters in path")
+                        )
+                    })?),
+                    Err(error) => match error.kind() {
+                        // Missing config in $HOME is not an error.
+                        io::ErrorKind::NotFound => Ok(None),
+                        _ => Err(error).context("Failed to read config file {config_path}"),
+                    },
+                }?
+            } else {
+                None
+            }
+        };
+
+        let api_key = env::var("OPENAI_API_KEY").or_else(|_| {
+            config
+                .as_ref()
+                .map(|c| c.api_key.clone())
+                .flatten()
+                .ok_or(anyhow!("Set `api_key` in config or `OPENAI_API_KEY` env."))
+        })?;
+
+        let api_url = url
+            .or_else(|| config.as_ref().map(|c| c.url.clone()).flatten())
+            .unwrap_or_else(|| String::from("https://models.inference.ai.azure.com/"));
+
+        let model = model
+            .or_else(|| config.as_ref().map(|c| c.model.clone()).flatten())
+            .unwrap_or_else(|| String::from("gpt-4o"));
+
+        let system_message =
+            system.or_else(|| config.as_ref().map(|c| c.system_message.clone()).flatten());
 
         Ok(Self {
             api_key,
-            api_url: url,
+            api_url,
             model,
-            system_message: system,
+            system_message,
         })
     }
 }
 
 fn main() -> anyhow::Result<()> {
-    let Configuration {
+    let AppConfiguration {
         api_key,
         api_url,
         model,
         system_message,
-    } = Configuration::init(Args::parse())?;
+    } = AppConfiguration::init(Args::parse())?;
 
     let mut chat = ChatClient::new(
         api_key,
