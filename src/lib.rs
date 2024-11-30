0589911fb330d1742f20b65ced0c5b06c6921a88
Dmitry Markin <dmitry@markin.tech>
2024-11-30 14:53:34 +0000

    minor: expose `Completion` type

--- src/lib.rs
+++ src/lib.rs
@@ -26,6 +26,6 @@
 
 mod chat_client;
 pub use chat_client::{
-    client::{ChatClient, ChatClientConfig, Error},
+    client::{ChatClient, ChatClientConfig, Completion, Error},
     openai_api::client::Auth,
 };
